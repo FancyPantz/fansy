86d10ec82a96b21938ed699a24006cce0ee754a3
FancyPantz <ayan.b@bronxsoftware.org>
2016-05-13 13:31:00 -0400

    Create lb3.js

---
+++ lb3.js
@@ -0,0 +1,47 @@
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+//face
+ctx.beginPath();
+ctx.arc(100,70,50,0,1*Math.PI);
+ctx.stroke();
+
+//right eye
+ctx.beginPath();
+ctx.arc(125,85,15,0,2*Math.PI);
+ctx.stroke();
+ctx.fill();
+
+//left eye
+ctx.beginPath();
+ctx.arc(75,85,15,0,2*Math.PI);
+ctx.stroke();
+ctx.fill();
+
+//mouth
+ctx.rect(91,100,20,10);
+ctx.stroke();
+
+//hat
+    ctx.fillStyle="black";    
+    ctx.beginPath();
+    ctx.moveTo(100,30);
+    ctx.lineTo(150,70);
+    ctx.lineTo(50,70);
+    ctx.fill();
+
+//hat top
+    ctx.fillStyle="black";
+    ctx.fillRect(65,30,70,30);
+    
+//bodytop
+    ctx.rotate(20*Math.PI/180);
+    ctx.beginPath();
+    ctx.arc(100,70,50,0,1*Math.PI);
+    ctx.stroke();
+    
+  }
+}
+
